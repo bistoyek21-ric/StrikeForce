b6c99371676abdf6becde5e280dbabdee8c4f0df
kasrafouladi <k4sr405@gmail.com>
2025-03-18 19:44:01 +0330

    License fixed

--- StrikeForce-client/enter.hpp
+++ StrikeForce-client/enter.hpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2024 bistoyek(21)
+Copyright (c) 2024 bistoyek21 R.I.C.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

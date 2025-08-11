14db06aeff65bd738cb3d095c642a04cb3d6515f
Kasra Fouladi <k4sr405@gmail.com>
2025-08-12 01:19:48 +0330

    Update basic.hpp

--- StrikeForce-client/basic.hpp
+++ StrikeForce-client/basic.hpp
@@ -38,7 +38,7 @@ SOFTWARE.
 
 #include "GraphicPrinter.hpp"
 
-#define COWRDSOURCED_TRAINING
+//#define COWRDSOURCED_TRAINING
 
 bool during_battle = false;
 
@@ -193,3 +193,4 @@ namespace Environment{
 
 	}
 }
+
